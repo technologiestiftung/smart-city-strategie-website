7bfc29f84dbd62a39b8cb1152fdaa51baaa73616
Lucas Vogel <contact@vogelino.com>
2021-02-17 12:35:48 +0100

    styles(header): Fix some responsive styles in header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,12 +4,12 @@ import { Logo } from "@components/Logo";
 
 export const Header: FC = () => (
   <>
-    <div className='fixed top-0 left-0 w-screen bg-white py-8 z-10'>
+    <div className='fixed top-0 left-0 w-screen bg-white py-4 md:py-6 z-10'>
       <div className='px-4 container mx-auto max-w-screen-lg z-10'>
         <Logo />
       </div>
     </div>
-    <div className='mb-20' style={{ marginTop: 160 }}>
+    <div className='mb-10 md:mb-20' style={{ marginTop: 144 }}>
       <Image
         src='/smart-city-header-cover_zn34yh.jpg'
         width='1920'
