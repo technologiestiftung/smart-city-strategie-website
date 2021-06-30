69436875b4747006b0729a7c0e6f866b93ef48f1
Lucas Vogel <contact@vogelino.com>
2021-06-30 17:20:20 +0200

    chore(matomo): add matomo script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import "react-notion-x/src/styles.css";
 import "../styles/globals.css";
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
 
 const App: FC<{
   Component: FC;
   pageProps: Record<string, unknown>;
 }> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (!MATOMO_URL || !MATOMO_SITE_ID) return;
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  });
+
   return <Component {...pageProps} />;
 };
 
